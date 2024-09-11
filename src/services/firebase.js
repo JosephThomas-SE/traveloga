8a795bb7b22dfec006e5a18289b186a4147af01b
Joseph Thomas <josephthomas0313@gmail.comm>
2024-09-12 00:41:13 +0530

    Added a firebase service to us the db, authentication, firestore and storage

---
+++ src/services/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const realtimeDb = getDatabase(app);
+
+export { app, auth, firestore, realtimeDb };
